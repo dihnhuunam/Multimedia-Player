8935134bc23469ac53c492a632d4a0dada4403ae
dihnhuunam <dihnhuunam@gmail.com>
2025-08-03 01:44:14 +0700

    [ADD]: Files and folders architecture

--- Code/View/Main.qml
+++ Code/View/Main.qml
@@ -1,27 +1,10 @@
 import QtQuick
-import QtQuick.Controls
-import QtQuick.Window
 
 Window {
-    id: window
+    id: window 
     width: 640
     height: 320
     visible: true
 
-    Column {
-        anchors.centerIn: parent
-        spacing: 10
-
-        TextField {
-            id: nameInput
-            width: 200
-            text: controller.name
-            placeholderText: "Enter your name"
-            onTextChanged: controller.name = text
-        }
-
-        Text {
-            text: "Name: " + (controller.name.length > 0 ? controller.name : "Empty")
-        }
-    }
+    
 }
--- Code/main.cpp
+++ Code/main.cpp
@@ -2,17 +2,17 @@
 #include <QQmlApplicationEngine>
 #include <QQmlContext>
 #include <QDebug>
-#include "Model.hpp"
-#include "Controller.hpp"
+#include "DatabaseAuthModel.hpp"
+#include "APIFactory.hpp"
 
 int main(int argc, char *argv[])
 {
     QGuiApplication app(argc, argv);
     QQmlApplicationEngine engine;
 
-    Model model;
-    Controller controller(model);
-    engine.rootContext()->setContextProperty("controller", &controller);
+    APIFactory *api = new APIFactory("http://localhost:3000/api");
+    IAuthModel *authModel = new DatabaseAuthModel(api);
+    authModel->loginUser("nam@gmail.com", "Nam123");
 
     const QUrl url("qrc:/View/Main.qml");
     QObject::connect(&engine, &QQmlApplicationEngine::objectCreated, &app, [url](QObject *obj, const QUrl &objUrl)
